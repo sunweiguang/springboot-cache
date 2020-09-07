31acb6c0c44a42bce82f1dd3b70b8ae6731306fb
sunweiguang <741289543@qq.com>
2020-09-07 11:23:22 +0800

    可以设置redis缓存的失效时间

--- src/main/java/com/tianqiauto/config/RedisConfig.java
+++ src/main/java/com/tianqiauto/config/RedisConfig.java
@@ -5,10 +5,18 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -32,5 +40,41 @@ public class RedisConfig {
       template.setKeySerializer(new StringRedisSerializer());
       template.afterPropertiesSet();
       return template;
+   }
+
+   @Bean
+   public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+      return new RedisCacheManager(
+              RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory),
+              this.getRedisCacheConfigurationWithTtl( -1), // 默认策略，未配置的 key 会使用这个
+              this.getRedisCacheConfigurationMap() // 指定 key 策略
+      );
+   }
+
+   private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap() {
+      Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+      //SsoCache和BasicDataCache进行过期时间配置
+      redisCacheConfigurationMap.put("messagCache", this.getRedisCacheConfigurationWithTtl(30 * 60));
+
+      //自定义设置缓存时间
+      redisCacheConfigurationMap.put("userCache", this.getRedisCacheConfigurationWithTtl(60));
+
+      return redisCacheConfigurationMap;
+   }
+
+   private RedisCacheConfiguration getRedisCacheConfigurationWithTtl(Integer seconds) {
+      Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+      ObjectMapper om = new ObjectMapper();
+      om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+      om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+      jackson2JsonRedisSerializer.setObjectMapper(om);
+      RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+      redisCacheConfiguration = redisCacheConfiguration.serializeValuesWith(
+              RedisSerializationContext
+                      .SerializationPair
+                      .fromSerializer(jackson2JsonRedisSerializer)
+      ).entryTtl(Duration.ofSeconds(seconds));
+
+      return redisCacheConfiguration;
    }
 }
